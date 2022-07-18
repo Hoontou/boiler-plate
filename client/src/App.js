31ad30b9ca2a5e8ea87f34bc29406916c2d2b00d
Hoontou <hoontou@gmail.com>
2022-07-19 00:58:02 +0900

    기본강의 완료, 리액트라우터돔v6에 맞게 hoc 수정까지.

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/views/LangindPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from './hoc/auth';
 
 function App() {
+  const AuthLandingPage = Auth(LandingPage, null);
+  const AuthLoginPage = Auth(LoginPage, false);
+  const AuthRegisterPage = Auth(RegisterPage, false);
+
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<LandingPage />}></Route>
-        <Route path='/login' element={<LoginPage />}></Route>
-        <Route path='/register' element={<RegisterPage />}></Route>
-      </Routes>
-    </Router>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<AuthLandingPage />} />
+          <Route path='/login' element={<AuthLoginPage />} />
+          <Route path='/register' element={<AuthRegisterPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
