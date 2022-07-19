5bc5515f615d68a9c67919fe2ec778bab6ee8244
Hoontou <hoontou@gmail.com>
2022-07-19 17:09:43 +0900

    last update

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -36,26 +36,21 @@ function LoginPage(props) {
   };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100vh',
-      }}
-    >
-      <form
-        style={{ display: 'flex', flexDirection: 'column' }}
-        onSubmit={onSubmitHandler}
-      >
-        <label>Email</label>
-        <input type='email' value={Email} onChange={onEmailHandler} />
-        <label>Password</label>
-        <input type='password' value={Password} onChange={onPasswordHandler} />
-        <br />
-        <button type='submit'>Login</button>
-      </form>
+    <div class='login-page'>
+      <div class='form'>
+        <form class='login-form' onSubmit={onSubmitHandler}>
+          <label>Email</label>
+          <input type='email' value={Email} onChange={onEmailHandler} />
+          <label>Password</label>
+          <input
+            type='password'
+            value={Password}
+            onChange={onPasswordHandler}
+          />
+          <br />
+          <button type='submit'>Login</button>
+        </form>
+      </div>
     </div>
   );
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { registerUser } from '../../../_actions/user_action';
+import './RegisterPage.css';
 
 function RegisterPage() {
   const dispatch = useDispatch();
@@ -50,38 +51,33 @@ function RegisterPage() {
     });
   };
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100vh',
-      }}
-    >
-      <form
-        style={{ display: 'flex', flexDirection: 'column' }}
-        onSubmit={onSubmitHandler}
-      >
-        <label>Email</label>
-        <input type='email' value={Email} onChange={onEmailHandler} />
+    <div class='login-page'>
+      <div class='form'>
+        <form class='login-form' onSubmit={onSubmitHandler}>
+          <label>Email</label>
+          <input type='email' value={Email} onChange={onEmailHandler} />
 
-        <label>Name</label>
-        <input type='text' value={Name} onChange={onNameHandler} />
+          <label>Name</label>
+          <input type='text' value={Name} onChange={onNameHandler} />
 
-        <label>Password</label>
-        <input type='password' value={Password} onChange={onPasswordHandler} />
+          <label>Password</label>
+          <input
+            type='password'
+            value={Password}
+            onChange={onPasswordHandler}
+          />
 
-        <label>Confirm Password</label>
-        <input
-          type='password'
-          value={ConfirmPassword}
-          onChange={onConfirmPasswordHandler}
-        />
+          <label>Confirm Password</label>
+          <input
+            type='password'
+            value={ConfirmPassword}
+            onChange={onConfirmPasswordHandler}
+          />
 
-        <br />
-        <button type='submit'>회원 가입</button>
-      </form>
+          <br />
+          <button type='submit'>회원 가입</button>
+        </form>
+      </div>
     </div>
   );
 }
